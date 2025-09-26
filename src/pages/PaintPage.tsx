8c9014217299695ea689322fb666776c240a3651
Rod <rsierraa@unal.edu.co>
2025-09-26 17:41:13 -0500

    Link to Profile in canvas view

--- src/pages/PaintPage.tsx
+++ src/pages/PaintPage.tsx
@@ -1,6 +1,7 @@
 // src/pages/PaintPage.tsx
 import React, { useState } from "react";
-import { Container, Row, Col } from "react-bootstrap";
+import { Container, Row, Col, Button } from "react-bootstrap";
+import { Link } from "react-router-dom";
 import Toolbar from "../components/Toolbar";
 import PropertiesPanel from "../components/PropertiesPanel";
 import Whiteboard from "../components/Whiteboard";
@@ -14,15 +15,18 @@ const PaintPage: React.FC = () => {
 
   return (
     <div className="paint-page">
+      <nav className="bg-light p-2 mb-3 d-flex justify-content-end align-items-center">
+        <Link to="/profile">
+          <Button variant="outline-primary" className="me-2">Perfil</Button>
+        </Link>
+      </nav>
       <Container fluid className="h-100">
         <Row className="h-100">
-
           <Col xs={10} className="canvas-col">
             <div className="stage-wrapper">
               <Whiteboard />
             </div>
           </Col>
-
         </Row>
       </Container>
     </div>
