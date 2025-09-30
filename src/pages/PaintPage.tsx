2196f133514c95cc4acf1803e63056f41079a246
cchois <94801991+cchois@users.noreply.github.com>
2025-09-29 21:33:09 -0500

    Add files via upload

--- src/pages/PaintPage.tsx
+++ src/pages/PaintPage.tsx
@@ -1,7 +1,6 @@
 // src/pages/PaintPage.tsx
 import React, { useState } from "react";
-import { Container, Row, Col, Button } from "react-bootstrap";
-import { Link } from "react-router-dom";
+import { Container, Row, Col } from "react-bootstrap";
 import Toolbar from "../components/Toolbar";
 import PropertiesPanel from "../components/PropertiesPanel";
 import Whiteboard from "../components/Whiteboard";
@@ -15,18 +14,15 @@ const PaintPage: React.FC = () => {
 
   return (
     <div className="paint-page">
-      <nav className="bg-light p-2 mb-3 d-flex justify-content-end align-items-center">
-        <Link to="/profile">
-          <Button variant="outline-primary" className="me-2">Perfil</Button>
-        </Link>
-      </nav>
       <Container fluid className="h-100">
         <Row className="h-100">
+
           <Col xs={10} className="canvas-col">
             <div className="stage-wrapper">
               <Whiteboard />
             </div>
           </Col>
+
         </Row>
       </Container>
     </div>
